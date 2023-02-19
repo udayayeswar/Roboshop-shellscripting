eee2136184a1f621a1f0d5463e4cae43e9d76af6
udayayeswari <udayeswari.savana@gmail.co>
2023-02-19 16:06:31 -0500

    appending the both output and error

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,19 @@
   echo Install nginx
-  yum install nginx -y
+  yum install nginx -y &>>/tmp/frontend
 
   echo downloading NIGINX web content
 
- curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
- cd /usr/share/nginx/html
+ curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/frontend
+ cd /usr/share/nginx/html &>>/tmp/frontend
 
  echo remaving old files
- rm -rf *
+ rm -rf * &>>/tmp/frontend
  echo unzip web content
- unzip /tmp/frontend.zip
+ unzip /tmp/frontend.zip &>>/tmp/frontend
 
- mv frontend-main/static/* .
- mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+ mv frontend-main/static/* . &>>/tmp/frontend
+ mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/frontend
 
  echo start NIGINX
-  systemctl enable nginx
- systemctl restart nginx
+  systemctl enable nginx &>>/tmp/frontend
+ systemctl restart nginx &>>/tmp/frontend
