62f18dc4e60a05ed137738c4ecf047d794aea9b7
udayayeswari <udayeswari.savana@gmail.co>
2023-02-19 23:23:09 -0500

    variable

--- frontend.sh
+++ frontend.sh
@@ -1,29 +1,30 @@
+  LOG_FILE=/tmp/frontend
   echo Install nginx
-  yum install nginx -y &>>/tmp/frontend
+  yum install nginx -y &>>$LOG-FILE
   echo status = $?
 
   echo downloading NIGINX web content
- curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/frontend
+ curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG-FILE
  echo status = $?
 
- cd /usr/share/nginx/html &>>/tmp/frontend
+ cd /usr/share/nginx/html &>>$LOG-FILE
  echo status = $?
 
- echo remaving old files
- rm -rf * &>>/tmp/frontend
+ echo removing old files
+ rm -rf * &>>$LOG-FILE
  echo status = $?
 
  echo unzip web content
- unzip /tmp/frontend.zip &>>/tmp/frontend
+ unzip /tmp/frontend.zip &>>$LOG-FILE
  echo status = $?
 
- mv frontend-main/static/* . &>>/tmp/frontend
+ mv frontend-main/static/* . &>>$LOG-FILE
  echo status = $?
 
- mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/frontend
+ mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG-FILE
 echo status = $?
 
  echo start NIGINX
-  systemctl enable nginx &>>/tmp/frontend
- systemctl restart nginx &>>/tmp/frontend
+  systemctl enable nginx &>>$LOG-FILE
+ systemctl restart nginx &>>$LOG-FILE
 echo status = $?
